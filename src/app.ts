6df2161a916fb96675e68309695bb12c2ff402df
Shourov Saha <shourovys@gmail.com>
2024-10-25 02:29:43 +0600

    build: deploy url add in cors

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://weather-dashboard-by-shourov.vercel.app',
+    ],
   })
 );
 
